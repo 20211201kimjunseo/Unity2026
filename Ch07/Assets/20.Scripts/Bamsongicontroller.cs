d9c45ca6773c81c39c724a0014a1ed5f6d48242b
geunjun <junseo4580@naver.com>
2026-05-04 10:49:21 +0900

    0504 Ch07 실습

--- Ch07/Assets/20.Scripts/Bamsongicontroller.cs
+++ Ch07/Assets/20.Scripts/Bamsongicontroller.cs
@@ -6,11 +6,17 @@ public class Bamsongicontroller : MonoBehaviour
     void Start()
     {
         Application.targetFrameRate = 60;
-        Shoot(new Vector3(0, 200, 2000));
+        //Shoot(new Vector3(0, 200, 2000));
     }
 
 public void Shoot(Vector3 dir)
     {
         GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
     }
 }
