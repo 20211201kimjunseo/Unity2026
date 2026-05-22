d0946bf480dbaa88d858e9b94f2966e2b56d0df5
geunjun <junseo4580@naver.com>
2026-05-22 20:26:26 +0900

    게임 만들기_1

---
+++ My Game/Assets/20.Scripts/ItemGenerator.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public GameObject bombPrefab;
+    public float span = 1f;
+    public int ratio = 3; // 30% Bomb
+    float delta = 0f;
+
+    void Update()
+    {
+        delta += Time.deltaTime;
+
+        if (delta > span)
+        {
+            GameObject item;
+
+            int dice = Random.Range(0, 10);
+            if (dice < ratio)
+            {
+                item = Instantiate(bombPrefab);
+            }
+            else
+            {
+                item = Instantiate(applePrefab);
+            }
+
+            float x = Random.Range(-1, 2);
+            float z = Random.Range(-1, 2);
+            item.transform.SetParent(transform);
+            item.transform.position = new Vector3(x, 7, z);
+            delta = 0f;
+        }
+    }
+}
---
+++ My Game/Assets/20.Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    GameObject director;
+    AudioSource aud;
+    Animator animator;
+    bool isHit = false;
+
+    void Start()
+    {
+
+        Application.targetFrameRate = 60;
+        aud = GetComponent<AudioSource>();
+        director = GameObject.Find("GameDirector");
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (isHit) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            int layerMask = ~LayerMask.GetMask("Player");
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0, z);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Apple")
+        {
+            Debug.Log("사과를 잡았다.");
+            aud.PlayOneShot(appleSE);
+            director.GetComponent<GameDirector>().GetApple();
+        }
+        else if (other.gameObject.tag == "Bomb")
+        {
+            Debug.Log("폭탄을 잡았다.");
+            aud.PlayOneShot(bombSE);
+            director.GetComponent<GameDirector>().GetBomb();
+            isHit = true;
+            animator.SetTrigger("Hit");
+            Invoke(nameof(RecoverFromHit), 2.0f);
+        }
+
+        Destroy(other.gameObject);
+    }
+
+    void RecoverFromHit()
+    {
+        isHit = false;
+    }
+}
